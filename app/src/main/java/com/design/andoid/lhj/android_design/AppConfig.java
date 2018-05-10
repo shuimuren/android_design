fffb956a89c70575f22f30fe448c7f47f3ec11ea
lianghongjing <lhj_1009@163.com>
2018-05-10 23:56:27 +0800

    网络请求搭建完毕

---
+++ app/src/main/java/com/design/andoid/lhj/android_design/AppConfig.java
@@ -0,0 +1,1 @@
+package com.design.andoid.lhj.android_design;/** * Created by lhj on 18/5/8 */public class AppConfig {}
---
+++ app/src/main/java/com/design/andoid/lhj/android_design/service/response/UserInfoResult.java
@@ -0,0 +1,1 @@
+package com.design.andoid.lhj.android_design.service.response;import com.design.andoid.lhj.android_design.resultbean.UserInfo;import com.design.andoid.lhj.android_design.service.BaseResult;import com.google.gson.annotations.SerializedName;/** * Created by lhj on 18/5/9 */public class UserInfoResult extends BaseResult<UserInfo> {}
