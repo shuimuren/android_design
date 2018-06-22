3307d69915df98699b17281dcdc0a1b8f8366fec
lianghongjing <lianghj@xiaomodo.com>
2018-06-22 12:20:29 +0800

    新增bugly统计

---
+++ app/src/main/java/com/lhj/android_design/bugly/BuglyContent.java
@@ -0,0 +1,14 @@
+package com.lhj.android_design.bugly;
+
+/**
+ * Create by xmd on 2018/6/22
+ * Describe:
+ */
+public class BuglyContent {
+
+    public static final String BUGLY_APP_KEY = "dfa5ba49-d3d9-45ba-82d6-d7e5fbd1f039";
+    public static final String BUGLY_APP_ID = "a5ef614387";
+    public static final boolean bugly_debug_true = true;
+    public static final boolean bugly_debug_false = false;
+
+}
---
+++ app/src/main/java/com/lhj/android_design/utils/AppUtil.java
@@ -0,0 +1,42 @@
+package com.lhj.android_design.utils;
+
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Create by xmd on 2018/6/22
+ * Describe: 系统相关工具类
+ */
+public class AppUtil {
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    public static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
